dbc6b57807ff63479a8fee96c910f46af79c32a9
Chiwai <chiwai.kiriba@gmail.com>
2025-05-16 12:56:58 +0300

    pause play

--- script.js
+++ script.js
@@ -4,20 +4,26 @@ const button = document.getElementById("button");
 async function selectMediaStream() {
   try {
     // Prompt to select media stream, pass to video element
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia({
-      video: true,
-      audio: true,
-    });
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     videoElement.srcObject = mediaStream;
     videoElement.onloadedmetadata = () => {
       videoElement.play();
     };
+    // Listen for the end of the stream
+    mediaStream.getVideoTracks()[0].addEventListener("ended", () => {
+      // Reset button
+      button.disabled = false;
+      // Stop the video element
+      videoElement.pause();
+      videoElement.srcObject = null;
+    });
   } catch (error) {
     console.error("Error accessing media devices.", error);
   }
 }
 
 button.addEventListener("click", async () => {
+  await selectMediaStream();
   // Disable button
   button.disabled = true;
   // Start picture in picture
